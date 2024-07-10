e0d7cc5c3dd2f2e48907de2d78f7cb5bfe4df7aa
Sujin1517 <11tnwls12@gmail.com>
2024-07-10 12:04:44 +0900

    [feat]: Create user table(#3)

---
+++ src/main/java/com/click/auth/domain/entity/User.java
@@ -0,0 +1,39 @@
+package com.click.auth.domain.entity;
+
+import com.click.auth.domain.type.UserIdentityType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "USERS")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "USER_ID")
+    private UUID userId;
+
+    @Column(name = "USER_IDENTITY", nullable = false)
+    private String userIdentity;
+
+    @Column(name = "USER_IDENTITY_TYPE", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserIdentityType userIdentityType;
+
+    @Column(name = "USER_NICK_NAME", nullable = false)
+    private String userNickName;
+
+    @Column(name = "USER_SIMPLE_PASSWD", nullable = false)
+    private String userPasswd;
+
+    @Column(name = "USER_CREDIT_RANK", columnDefinition = "700")
+    private Integer userCreditRank;
+
+    @Column(name = "USER_DISABLE", columnDefinition = "boolean default false")
+    private Boolean isDisable;
+}
