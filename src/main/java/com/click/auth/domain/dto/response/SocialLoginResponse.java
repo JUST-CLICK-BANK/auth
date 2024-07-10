6aa15c327bb708d9065e3c2e1fa069231b82a1e0
Sujin1517 <11tnwls12@gmail.com>
2024-07-10 19:16:34 +0900

    [Fix]: Fix Kakao login return(#3)

---
+++ src/main/java/com/click/auth/domain/dto/response/SocialLoginResponse.java
@@ -0,0 +1,26 @@
+package com.click.auth.domain.dto.response;
+
+import com.click.auth.domain.type.UserIdentityType;
+import io.jsonwebtoken.Claims;
+
+public record SocialLoginResponse(
+        Boolean isAlready,
+        String identity,
+        UserIdentityType type
+) {
+    public static SocialLoginResponse fromKakao(KakaoTokenInfoResponse res, boolean isAleady) {
+        return new SocialLoginResponse(
+                isAleady,
+                res.id().toString(),
+                UserIdentityType.KAKAO
+        );
+    }
+
+    public static SocialLoginResponse fromToken(Claims claims){
+        return new SocialLoginResponse(
+            false,
+                claims.get("identity", String.class),
+                claims.get("type", UserIdentityType.class)
+        );
+    }
+}
