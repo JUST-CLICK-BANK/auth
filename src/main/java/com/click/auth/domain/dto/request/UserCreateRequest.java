9cd49ad88ecebd12c2e95836dbb353a0e8a6a8ba
Sujin1517 <11tnwls12@gmail.com>
2024-07-11 12:11:25 +0900

    [Feat]: Add toEntity in userCreateReq(#3)

--- src/main/java/com/click/auth/domain/dto/request/UserCreateRequest.java
+++ src/main/java/com/click/auth/domain/dto/request/UserCreateRequest.java
@@ -1,6 +1,9 @@
 package com.click.auth.domain.dto.request;
 
+import com.click.auth.domain.entity.User;
 import com.click.auth.domain.type.UserIdentityType;
+
+import java.time.LocalDateTime;
 
 public record UserCreateRequest(
         String identity,
@@ -8,4 +11,17 @@ public record UserCreateRequest(
         String nickname,
         String passwd
 ) {
+    public User toEntity() {
+        return new User(
+                null,
+                identity,
+                type,
+                nickname,
+                passwd,
+                700,
+                LocalDateTime.now(),
+                1,
+                false
+        );
+    }
 }
